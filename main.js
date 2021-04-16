dd5c9e230b49cbfa1d4b1bcd1edae3e513bbf585
MatisseBlasco <matisseblasco1999@gmail.com>
2021-04-16 09:49:03 +0200

    scroll-top

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
